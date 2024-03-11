c37843bb9d01a07e5316603a7894638926cc091c
bisordesolidus <151962055+bisordesolidus@users.noreply.github.com>
2024-03-11 12:02:00 +0300

    Update sort_function.h

--- sort_function.h
+++ sort_function.h
@@ -4,3 +4,4 @@ void quick_sort(int* mas, int size);
 void buble_sort(int* mas, int size);
 void choice_sort(int* mas, int size);
 void insert_sort(int* mas, int size);
+void heapSort(int* mas, int n);
